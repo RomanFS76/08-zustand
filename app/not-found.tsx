f5a03e84b9c8810c44aaf2b00a191b59c09eb8b5
RomanFS76 <korvy85@gmail.com>
2026-01-26 19:15:07 +0300

    feat: add metadata for not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,27 @@
 import css from './Home.module.css'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Page not found | NoteHub',
+  description:
+    'The page you are looking for does not exist.',
+  openGraph: {
+    title: 'Page not found | NoteHub',
+    description:
+      'The page you are looking for does not exist.',
+    url: 'https://notehub.io/404',
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub - Page not found',
+      },
+    ],
+    type: 'website',
+  },
+};
 
 const NotFound = () => {
   return (
